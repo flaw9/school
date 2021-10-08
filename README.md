aeaff62840636ae3d1c0974331b20e60e7bbdbed
Flaw_9 <contact@flaw.dev>
2021-10-08 14:09:13 +0200

    TD 2.2 ^^

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# school
+# School
+
 This is were I'll share all the courses, lessons and works I'm having, so my classmates can be inspired ;)
+
+> Please note that it is not recommended to copy and paste my code without understanding it.
+
+In (initDev)[/initDev] you can find all the files related to the R1.01.
+
+~ Flaw_9
---
+++ initDev/td2.2.py
@@ -0,0 +1,35 @@
+from math import sqrt
+
+def resolve(a, b, c):
+    s = []
+    if a == 0:
+        if b == 0:
+            if c == 0:
+                ns = -1
+        else:
+            s.append( (-c) / b )
+            ns = 1
+    else:
+        delta = b**2 / (4 * a * c)
+        if delta > 0:
+            s.append( (b - sqrt(delta)) / (2 * a) )
+            s.append( (b + sqrt(delta)) / (2 * a) )
+            ns = 2
+        else:
+            s.append( (-b) / (2 * a) )
+    return ns, s
+
+def main():
+    """Récupération de toutes les variables, appel de la fonction de résolution et affichage du résultat."""
+    a = int(input("Entrez le coefficient de degré 2: "))
+    b = int(input("Entrez le coefficient de degré 1: "))
+    c = int(input("Entrez le coefficient constant: "))
+    resultat = resolve(a, b, c)
+    print(f"L'équation possède {resultat[0]} solution(s).")
+    if len(resultat[1]) > 0:
+        for i in range(len(resultat[1])):
+            print(f"Résultat n°{i+1} = {resultat[1][i]}")
+
+
+if __name__ == "__main__":
+    main()
