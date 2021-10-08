148904fbeeea09fdcb04914b43889e74754f6861
Flaw_9 <contact@flaw.dev>
2021-10-08 14:30:50 +0200

    more python

---
+++ initDev/td2.2.2.py
@@ -0,0 +1,20 @@
+def fdsc(t):
+    '''fdsc = Fonction Du Second Degré'''
+    return 2 * t**3 - t**2 - 37 * t + 36
+
+def main():
+    t = -5
+    minimum = fdsc(t)
+    maximum = minimum
+    while t < 5:
+        t += 0.25
+        f = fdsc(t)
+        if f < minimum:
+            minimum = f
+        elif f > maximum:
+            maximum = f
+    print(f"La valeur minimale est: {minimum}")
+    print(f"La valeur maximale est: {maximum}")
+
+if __name__ == "__main__":
+    main()
---
+++ initDev/td2.2.3.py
@@ -0,0 +1,21 @@
+def methode_1():
+    x = 1
+    while x != 0:
+        x = -1
+        while x < 0:
+            x = int(input("Entrez une valeur: "))
+        if x > 0:
+            a = int(x)
+            for i in range(1, 100):
+                a = 0.5 * (a + x/a)
+            print(f"La racine carrée de {x} est {a}.")
+
+def methode_2():
+    pass
+
+def main():
+    methode_1()
+    methode_2()
+
+if __name__ == "__main__":
+    main()
