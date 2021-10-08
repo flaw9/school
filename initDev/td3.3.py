c34368d561259130fff2f2eb6fb561ff8450d51f
Flaw_9 <contact@flaw.dev>
2021-10-08 17:36:40 +0200

    Update td3.3.py

--- initDev/td3.3.py
+++ initDev/td3.3.py
@@ -7,6 +7,7 @@ def main():
     pos = 0
     while pos < 1 or pos > len(t):
         pos = int(input("Saisir une position d'insertion (1 < pos < taille): "))
+        pos -= 1
     for i in range(len(t)-1, pos-1, -1):
         t[i] = t[i-1]
     t[pos] = nb
