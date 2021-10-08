a7ba63e2625f0c5399d4d1fc1cdf090235902d3b
Flaw_9 <contact@flaw.dev>
2021-10-08 16:53:25 +0200

    updates

--- initDev/td3.4.py
+++ initDev/td3.4.py
@@ -7,10 +7,9 @@ def main():
     pos = 0
     while pos < 1 or pos > len(t):
         pos = int(input("Saisir la position de l'élément à supprimer: "))
-    i = int(pos)
-    while i < len(t)-1:
+    for i in range(pos, len(t)-1):
         t[i] = t[i+1]
-        i += 1
+    t.pop()
     print(t)
 
 if __name__ == "__main__":
---
+++ initDev/td3.5.py
@@ -0,0 +1,8 @@
+def main():
+    nb = int(input("Saisir un nombre à supprimer: "))
+    noc = 0
+    for i in range(0, len(t)):
+        pass
+
+if __name__ == "__main__":
+    main()
