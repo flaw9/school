ec5fedac69591c9823bb4422bdc325ffc8425e6d
Flaw_9 <contact@flaw.dev>
2021-10-08 14:50:32 +0200

    woops I was wrong

--- initDev/td2.2.3.py
+++ initDev/td2.2.3.py
@@ -6,16 +6,37 @@ def methode_1():
             x = int(input("Entrez une valeur: "))
         if x > 0:
             a = int(x)
-            for i in range(1, 100):
+            for i in range(1, 101):
                 a = 0.5 * (a + x/a)
             print(f"La racine carrée de {x} est {a}.")
 
 def methode_2():
-    x = int(input())
+    '''J'ai pas réellement compris l'intérêt ni le fonctionnement de cette fonction mais bon...'''
+    x = int(input("Entrez une valeur: "))
+    while x < 0:
+        x = int(input("Entrez une valeur: "))
+    if x != 0:
+        a = int(x)
+        for i in range(1, 101):
+            a = 0.5 * (a + x/a)
+        print(f"La racine carrée de {x} est {a}.")
+    while x != 0:
+        x = int(input("Entrez une valeur: "))
+        while x < 0:
+            x = int(input("Entrez une valeur: "))
+        if x != 0:
+            a = int(x)
+            for i in range(1, 101):
+                a = 0.5 * (a + x/a)
+            print(f"La racine carrée de {x} est {a}.")
+
+def methode_3():
+    pass
 
 def main():
     # methode_1()
-    methode_2()
+    # methode_2() # je ne sais pas ce que c'est que cette fonction ...
+    methode_3()
 
 if __name__ == "__main__":
     main()
--- initDev/td2.2.py
+++ initDev/td2.2.py
@@ -10,14 +10,14 @@ def resolve(a, b, c):
             s.append( (-c) / b )
             ns = 1
     else:
-        delta = b**2 / (4 * a * c)
+        delta = b**2 - (4 * a * c)
         if delta > 0:
-            s.append( (b - sqrt(delta)) / (2 * a) )
-            s.append( (b + sqrt(delta)) / (2 * a) )
+            s.append( - (b - sqrt(delta)) / (2 * a) )
+            s.append( - (b + sqrt(delta)) / (2 * a) )
             ns = 2
         else:
             s.append( (-b) / (2 * a) )
-    return ns, s
+    return (ns if ns!=None else 0), s
 
 def main():
     """Récupération de toutes les variables, appel de la fonction de résolution et affichage du résultat."""
@@ -26,9 +26,11 @@ def main():
     c = int(input("Entrez le coefficient constant: "))
     resultat = resolve(a, b, c)
     print(f"L'équation possède {resultat[0]} solution(s).")
-    if len(resultat[1]) > 0:
+    if resultat[0] > 0:
         for i in range(len(resultat[1])):
             print(f"Résultat n°{i+1} = {resultat[1][i]}")
+    else:
+        print(f"L'équation n'a pas de solution.")
 
 
 if __name__ == "__main__":
