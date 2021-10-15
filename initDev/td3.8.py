4a054ae9aca1d5434bff50bcee6fa282a703aa4a
Flaw_9 <contact@flaw.dev>
2021-10-15 14:34:19 +0200

    Update td3.8.py

--- initDev/td3.8.py
+++ initDev/td3.8.py
@@ -1,9 +1,16 @@
 def main():
-    n = int(input("Combien de caractères ?: "))
     chaine = list(map(str, input("Chaine ade caractères à coder: ")))
     chaine2 = ""
     for i in chaine:
-        chaine2 = f"{chaine2}{chr( ord(i) + n)}"
+        if i != ' ' and i != 'Z' and i != 'z':
+            chaine2 = f"{chaine2}{chr( ord(i) + 1)}"
+        elif i == 'z':
+            chaine2 = f"{chaine2}a"
+        elif i == 'Z':
+            chaine2 = f"{chaine2}A"
+        else:
+            chaine2 = f"{chaine2} "
+
     print(chaine2)
 
 if __name__ == "__main__":
