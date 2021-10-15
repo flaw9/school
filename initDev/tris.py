7cbf3dbcdc8d56d67568fc6ce61f728d407f819f
Flaw_9 <contact@flaw.dev>
2021-10-15 17:42:17 +0200

    Update tris.py

--- initDev/tris.py
+++ initDev/tris.py
@@ -1,4 +1,4 @@
-def tri_bulles(array):
+def tri_bulles(array: list):
     for i in range(1,len(array)+1):
         for j in range(1,len(array)-i +1):
             if array[j-1] > array[j]:
@@ -14,10 +14,20 @@ def tri_insertion(array: list):
         array.insert(pos, array.pop(i))
     return array
 
+def tri_selection(array: list):
+    for i in range(len(array)):
+        mini = i
+        for j in range(i, len(array)):
+            if array[j] < array[mini]:
+                mini = j
+        array[mini], array[i] = array[i], array[mini]
+    return array
+
 def main():
     t = list(map(int, input().split()))
     # print(tri_bulles(t))
     # print(tri_insertion(t))
+    # print(tri_selection(t))
 
 if __name__ == "__main__":
     main()
