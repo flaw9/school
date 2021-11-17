e68c605e484ab8d6785c8100f99860d639a66f44
flaw9 <contact@flaw.dev>
2021-11-17 08:58:09 +0100

    ...

---
+++ initDev/C_course/taille_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Taille d'un char : %lu\n", sizeof(char));
+    printf("Taille d'un unsigned char : %lu\n", sizeof(unsigned char));
+    printf("Taille d'un short : %lu\n", sizeof(short));
+    printf("Taille d'un unsigned short : %lu\n", sizeof(unsigned short));
+    printf("Taille d'un int : %lu\n", sizeof(int));
+    printf("Taille d'un unsigned int : %lu\n", sizeof(unsigned int));
+    printf("Taille d'un long int : %lu\n", sizeof(long int));
+    printf("Taille d'un long long int : %lu\n", sizeof(long long int));
+    printf("Taille d'un float : %lu\n", sizeof(float));
+    printf("Taille d'un double : %lu\n", sizeof(double));
+    printf("Taille d'un long double : %lu\n", sizeof(long double));
+}
