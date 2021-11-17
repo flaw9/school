adf6818658f460459968cf864553e3b32f9a8576
flaw9 <contact@flaw.dev>
2021-11-17 10:48:23 +0100

    ...

---
+++ initDev/C_course/tableaux_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float tab[100]:
+    int i;
+    for (i=0; i <100; i++) {
+        tab[i] = (float) i/100;
+        printf("Valeur insere en position %d : %f\n", i, tab[i]);
+    }
+    return 0;
+}
