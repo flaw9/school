607874852bb9812f6fa1d029d8d86f7dc839ddf3
flaw9 <contact@flaw.dev>
2021-11-17 10:04:24 +0100

    ...

---
+++ initDev/C_course/do_while_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i=0;
+    i=0;
+    while (i < 0) {
+        printf("(1)%02de instruction de la boucle\n", i);
+    }
+    i=0;
+    do {
+        printf("(2)%02de instruction de la boucle\n", i);
+    } while (i < 0);
+    return 0;
+}
