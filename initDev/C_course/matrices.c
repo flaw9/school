1c491ad43c75d471265ef9dfbf5e4e75b0af6b3d
flaw9 <contact@flaw.dev>
2021-11-17 11:28:41 +0100

    ...

--- initDev/C_course/matrices.c
+++ initDev/C_course/matrices.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 
+void saisiMat() {
+    int lar, lon;
+    printf("Saisir la largeur: ");
+    scanf("%d", &lar)
+    printf("Saisir la longueur: ");
+    scanf("%d", &lon)
+    float tab1[lon][lar];
+    float tab2[lon][lar];
+    return tab1, tab2;
+}
+
+void afficherMat(float mat[]) {
+    for(i=0;i<sizeof(mat),i++) {
+        printf("|");
+        for(o=0;o<sizeof(mat[i]);o++) {
+            printf("%f", mat[i][o]);
+        }
+        printf("|\n");
+    }
+}
+
 int main() {
     int N;
     printf("Saisir N: ");
     scanf("%d",&N);
 
-    float tab[N][N];
+    float tab1[N][N];
+    float tab2[N][N];
+    float tab3[N][N];
     
-    printf("Matrice générée.\n");
+    printf("Matrices générées.\n");
+    printf("Matrice n°1: \n");
+    afficherMat(tab1);
 }
