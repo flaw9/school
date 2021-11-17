12221123d5702615282397af13c56b3c47e820df
flaw9 <contact@flaw.dev>
2021-11-17 11:17:49 +0100

    ...

--- initDev/C_course/matrices.c
+++ initDev/C_course/matrices.c
@@ -4,7 +4,11 @@ int main() {
     int N;
     printf("Saisir N: ");
     scanf("%d",&N);
-    
 
     float tab[N];
+
+    for(i=0; i<N; i++)
+        tab[N] = float tab[N];
+    
+    printf("Matrice générée.\n");
 }
