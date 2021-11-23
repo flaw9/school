5d4ffcd68fd534d576afe6e28f6343cda859527d
flaw9 <contact@flaw.dev>
2021-11-23 16:33:09 +0100

    ...

--- initDev/C_course/tp16-17/readDB.c
+++ initDev/C_course/tp16-17/readDB.c
@@ -2,26 +2,18 @@
 
 
 int main(int argc,char *argv[])
-{
-    
-    
+{   
     FILE *file;
 
-    int nbPeople=0,nbHommes=0,nbFemmes=0,ageTotal=0;
-    file = fopen("DB.txt","r");
-    char name[100];
-    char firstName[100];
-    int age;
-    char type;
+    int nbPeople=0,nbHommes=0,nbFemmes=0,ageTotal=0; // Initialisation des variables qui serviront à compter.
+    file = fopen("DB.txt","r"); // Ouverture du fichier dans une liste.
+    char name[100]; char firstName[100]; int age; char type; // Initialisation des variables qui serviront à séparer en plusieurs champs les lignes.
 
-    while (fscanf(file,"%s %s %d %c",name,firstName,&age,&type) == 4){
-        if(type == 'M') nbHommes++; else if (type == 'F') nbFemmes++;
-        ageTotal += age;
-        printf("First name: %s ; Age: %d\n", firstName, age);
-        nbPeople++;
+    while (fscanf(file,"%s %s %d %c",name,firstName,&age,&type) == 4){ // On parcours le fichier ligne par ligne.
+        if(type == 'M') nbHommes++; else if (type == 'F') nbFemmes++; // Incrémentation du compteur correspondant au sexe de l'individu.
+        ageTotal += age; nbPeople++; // Incrémentation des variables nécessaires au calcul de l'âge moyen.
     }
-    printf("ageTotal: %d\nnbPeople: %d\n", ageTotal, nbPeople);
-    float avgAge = (float)ageTotal / nbPeople;
+    float avgAge = (float)ageTotal / nbPeople; // Calcul de l'âge moyen
     printf("Le fichier contient %d noms de personnes, dont %d hommes et %d femmes, avec un âge moyen de %f\n",nbPeople,nbHommes,nbFemmes,avgAge);
     fclose(file);
     return 0;
