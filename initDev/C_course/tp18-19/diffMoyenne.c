2db1eccd59a14864f408955870891a7d5b2695dd
flaw9 <contact@flaw.dev>
2021-11-30 17:05:00 +0100

    ...

--- initDev/C_course/tp18-19/diffMoyenne.c
+++ initDev/C_course/tp18-19/diffMoyenne.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int nbElements;
-    printf("Nombre d'elements: ");
-    scanf("%d", &nbElements);
+    // int nbElements;
+    // printf("Nombre d'elements: ");
+    // scanf("%d", &nbElements);
 
-    float tab[nbElements];
+    // float tab[nbElements];
 
-    printf("Valeurs: ");
-    for (int i = 0; i < nbElements; i++) {
-        scanf("%ld", &tab[i]);
-    }
+    // printf("Valeurs: ");
+    // for (int i = 0; i < nbElements; i++) {
+    //     scanf("%ld", &tab[i]);
+    // }
 
-    for (int i = 0; i < nbElements; i++) {
-        printf("%ld", tab[i]);
-    }
+    // for (int i = 0; i < nbElements; i++) {
+    //     printf("%ld", tab[i]);
+    // }
+    printf("Ne fonctionne pas !\n");
 }
