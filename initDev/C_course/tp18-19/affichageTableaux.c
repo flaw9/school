8cebd97d2682b240a1698b3e104ad6b97cf89fd9
flaw9 <contact@flaw.dev>
2021-11-30 14:23:15 +0100

    ...

--- initDev/C_course/tp18-19/affichageTableaux.c
+++ initDev/C_course/tp18-19/affichageTableaux.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+#define MAX 6
+
 int main()
 {
     int chiffres[6] = {1, 2, 3, 4, 5, 6}:
     float x[5] = {0.14, 1.22, 3, 4.5, 78};
     char couleur[3] = {’R, ’V’, ’B’};
 
-    
+    for(int i = 0; i < MAX; i++)
+    {
+        if(i < sizeof(chiffres)) {
+            printf("%i", chiffres[i]);
+        }
+        if(i < sizeof(x)) {
+            printf("%f", x[i]);
+        }
+        if(i < sizeof(couleur)) {
+            printf("%d", couleur[i]);
+        }
+        printf("\n");
+    }
 }
