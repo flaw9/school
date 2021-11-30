5f8197ebb9b4a67c10d7354793a97a6a8cdcf135
flaw9 <contact@flaw.dev>
2021-11-30 16:17:42 +0100

    ...

---
+++ initDev/C_course/tp18-19/exemple.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void afficherTab(char *tab) {
+    int i;
+    for(i=0;i<sizeof(tab);i++) {
+        printf("%d", tab[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int tab[] = { [0] = 42, [1] = 64, [99] = 100 };
+    afficherTab(tab);
+}
+
