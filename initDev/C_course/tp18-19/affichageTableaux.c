608296e31008a788e59ccb2fae1ba9637f360e33
flaw9 <contact@flaw.dev>
2021-11-30 15:03:03 +0100

    ...

--- initDev/C_course/tp18-19/affichageTableaux.c
+++ initDev/C_course/tp18-19/affichageTableaux.c
@@ -39,13 +39,10 @@ int main() {
     int chiffres[6] = {1, 2, 3, 4, 5, 6};
     float x[5] = {0.14, 1.22, 3, 4.5, 78};
     char couleur[3] = {'R', 'V', 'B'};
-
-    for (int i = 0; i < 3; i++) {
-        for (int o = 0; o < 6; o++) {
-            printf("chiffres[%i] = %i", i, chiffres[i]);
-            if (i < 5) printf("x[%i] = %f", i, x[i]);
-            if (i < 3) printf("couleur[%i] = %c", i, couleur[i]);
-        }
+    for (int o = 0; o < 6; o++) {
+        printf("chiffres[%i] = %i", i, chiffres[i]);
+        if (i < 5) printf("\tx[%i] = %f", i, x[i]);
+        if (i < 3) printf("\tcouleur[%i] = %c", i, couleur[i]);
         printf("\n");
     }
 }
