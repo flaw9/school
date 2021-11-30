ddd5c0d7128b580c6ab1c228c7ae3ce9ad5d5809
flaw9 <contact@flaw.dev>
2021-11-30 14:23:43 +0100

    ...

--- initDev/C_course/tp18-19/affichageTableaux.c
+++ initDev/C_course/tp18-19/affichageTableaux.c
@@ -6,7 +6,7 @@ int main()
 {
     int chiffres[6] = {1, 2, 3, 4, 5, 6}:
     float x[5] = {0.14, 1.22, 3, 4.5, 78};
-    char couleur[3] = {’R, ’V’, ’B’};
+    char couleur[3] = {'R', 'V', 'B'};
 
     for(int i = 0; i < MAX; i++)
     {
