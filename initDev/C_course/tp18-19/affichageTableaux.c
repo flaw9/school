71a02429be3c25e8f72026f46fe6747076f4a945
flaw9 <contact@flaw.dev>
2021-11-30 15:00:57 +0100

    ...

--- initDev/C_course/tp18-19/affichageTableaux.c
+++ initDev/C_course/tp18-19/affichageTableaux.c
@@ -36,7 +36,7 @@
 // }
 
 int main() {
-    int chiffres[6] = {1, 2, 3, 4, 5, 6}:
+    int chiffres[6] = {1, 2, 3, 4, 5, 6};
     float x[5] = {0.14, 1.22, 3, 4.5, 78};
     char couleur[3] = {'R', 'V', 'B'};
 
