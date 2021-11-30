d0525783f89d283c624f28d3921fcd0cd99d9654
flaw9 <contact@flaw.dev>
2021-11-30 14:50:50 +0100

    ...

--- initDev/C_course/tp18-19/affichageTableaux.c
+++ initDev/C_course/tp18-19/affichageTableaux.c
@@ -1,38 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
 #define SIZE1 abs(rand() % 10 + 1)
 #define SIZE2 abs(rand() % 10 + 1)
 
 int main()
 {
-    int maxSize = SIZE1;
-    if(SIZE2 > maxSize) maxSize = SIZE2;
-
     int chiffres[SIZE1];
-    int x[SIZE2];
-    int couleur[3] = {'R', 'V', 'B'};
-
-    for(int i = 0; i < 2; i++) {
-        if(i == 0) {
-            for(int o = 0; o < SIZE1; o++)
-                chiffres[o] = abs(rand() % 10 + 1);
-        } else if(i == 1) {
-            for(int o = 0; o < SIZE2; o++)
-                x[o] = abs(rand() % 100 / 10);
-        }
+    for (int i = 0; i < SIZE1; i++) {
+        chiffres[i] = abs(rand() % 10 + 1);
     }
+    float x[SIZE2];
+    for (int i = 0; i < SIZE1; i++) {
+        x[i] = abs(rand() % 100 / 10);
+    }
+    char couleur[3] = {'R', 'V', 'B'};
 
-    for(int i = 0; i < maxSize; i++)
+    printf("%i\n", sizeof(x));
+    for(int i = 0; i < 6; i++)
     {
         if(i < SIZE1) {
             printf("chiffres[%i] = %i", i, chiffres[i]);
         }
-        if(i < SIZE2 && x[i] != 0) {
-            printf("\t x[%i] = %f", i, x[i]);
+        if(i < SIZE2) {
+            printf("\tx[%i] = %f", i, x[i]);
         }
-        if(i < sizeof(couleur)) {
-            printf("\t couleur[%i] = '%c'", i, couleur[i]);
+        if(i < 3) {
+            printf("\tcouleur[%i] = '%c'", i, couleur[i]);
         }
         printf("\n");
     }
