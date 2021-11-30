77153ab24214b4b94daebc01149b439d257d5caa
flaw9 <contact@flaw.dev>
2021-11-30 14:27:43 +0100

    ...

--- initDev/C_course/tp18-19/affichageTableaux.c
+++ initDev/C_course/tp18-19/affichageTableaux.c
@@ -8,6 +8,7 @@ int main()
     float x[5] = {0.14, 1.22, 3, 4.5, 78};
     char couleur[3] = {'R', 'V', 'B'};
 
+    printf("%i", sizeof(x));
     for(int i = 0; i < MAX; i++)
     {
         if(i < sizeof(chiffres)) {
@@ -17,7 +18,7 @@ int main()
             printf("\tx[%i] = %f", i, x[i]);
         }
         if(i < sizeof(couleur)) {
-            printf("\tcouleur[%i] = %c", i, couleur[i]);
+            printf("\tcouleur[%i] = '%c'", i, couleur[i]);
         }
         printf("\n");
     }
