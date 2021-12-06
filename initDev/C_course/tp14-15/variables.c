353ea3f84014366d46b8e867f1c56c6c8d287fbf
flaw9 <contact@flaw.dev>
2021-12-06 14:25:13 +0100

    ...

--- initDev/C_course/tp14-15/variables.c
+++ initDev/C_course/tp14-15/variables.c
@@ -14,12 +14,12 @@ int main() {
 
     printf("char\t\t: valeur=%c, taille=%ld\n", c, sizeof(c));
     printf("unsigned char\t\t: valeur=%d, taille=%ld\n", uc, sizeof(uc));
-    printf("short\t\t: valeur=%d, taille=%ld\n", c, sizeof(s));
-    printf("unsigned short\t\t: valeur=%d, taille=%ld\n", c, sizeof(us));
-    printf("int\t\t: valeur=%d, taille=%ld\n", c, sizeof(i));
-    printf("unsigned int\t\t: valeur=%d, taille=%ld\n", c, sizeof(ui));
-    printf("long int\t\t: valeur=%u, taille=%ld\n", c, sizeof(li));
-    printf("float\t\t: valeur=%ld, taille=%ld\n", c, sizeof(f));
-    printf("double\t\t: valeur=%f, taille=%ld\n", c, sizeof(d));
-    printf("long double\t\t: valeur=%f, taille=%ld\n", c, sizeof(ld));
+    printf("short\t\t: valeur=%d, taille=%ld\n", s, sizeof(s));
+    printf("unsigned short\t\t: valeur=%d, taille=%ld\n", us, sizeof(us));
+    printf("int\t\t: valeur=%d, taille=%ld\n", i, sizeof(i));
+    printf("unsigned int\t\t: valeur=%d, taille=%ld\n", ui, sizeof(ui));
+    printf("long int\t\t: valeur=%u, taille=%ld\n", li, sizeof(li));
+    printf("float\t\t: valeur=%ld, taille=%ld\n", f, sizeof(f));
+    printf("double\t\t: valeur=%f, taille=%ld\n", d, sizeof(d));
+    printf("long double\t\t: valeur=%f, taille=%ld\n", ld, sizeof(ld));
 }
