719e2ebf832ff53048592985fc8b15e195a9749f
flaw9 <contact@flaw.dev>
2021-12-14 12:21:23 +0100

    ...

--- initDev/C_course/td13/piles.c
+++ initDev/C_course/td13/piles.c
@@ -52,7 +52,7 @@ int sommet(PILE *p)
 void afficherEtat(PILE *p) {
     printf("\n");
     printf("*État de la pile*\nContenu de la pile: \n");
-    for(int i = TAILLEMAX-1; i > 0; i--) {
+    for(int i = TAILLEMAX-1; i >= 0; i--) {
         printf("%d : %d\n", i+1, p->pile[i]);
     }
     (pilePleine(p) == 0) ? printf("La pile n'est pas pleine !\n") : printf("La pile est pleine !\n");
