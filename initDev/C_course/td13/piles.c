f1af885123e0bbad81c51f54996f5e1e12a479c9
flaw9 <contact@flaw.dev>
2021-12-14 15:58:35 +0100

    ...

--- initDev/C_course/td13/piles.c
+++ initDev/C_course/td13/piles.c
@@ -11,15 +11,15 @@ struct pile {
 typedef struct pile PILE;
 
 /* Renvoie 1 si la pile est vide, sinon 0. */
-int pileVide(PILE *p)
+int pileVide(PILE p)
 {
-    return p->sommet == -1 ? 1 : 0;
+    return p.sommet == -1 ? 1 : 0;
 }
 
 /* Renvoie 1 si la pile est pleine, sinon 0. */
-int pilePleine(PILE *p)
+int pilePleine(PILE p)
 {
-    return (p->sommet == TAILLEMAX-1) ? 1 : 0;
+    return (p.sommet == TAILLEMAX-1) ? 1 : 0;
 } 
 
 /* Si la pile n'est pas vide, renvoie le dernier élément et décrémente le sommet. */
@@ -47,9 +47,9 @@ int empiler(PILE *p, int elt)
     }
 }
 
-int sommet(PILE *p)
+int sommet(PILE p)
 {
-    return p->pile[p->sommet];
+    return p.pile[p.sommet];
 }
 
 void afficherEtat(PILE *p) {
