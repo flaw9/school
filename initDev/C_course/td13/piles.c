1b130e1c9bf1b85d5e137b29debb23fb949cf9df
flaw9 <contact@flaw.dev>
2021-12-14 12:09:44 +0100

    ...

--- initDev/C_course/td13/piles.c
+++ initDev/C_course/td13/piles.c
@@ -11,21 +11,21 @@ struct pile {
 typedef struct pile PILE;
 
 /* Renvoie 1 si la pile est vide, sinon 0. */
-int pileVide(PILE p)
+int pileVide(PILE *p)
 {
     return p.sommet == -1 ? 1 : 0;
 }
 
 /* Renvoie 1 si la pile est pleine, sinon 0. */
-int pilePleine(PILE p)
+int pilePleine(PILE *p)
 {
     return (p.sommet == TAILLEMAX) ? 1 : 0;
 } 
 
 /* Si la pile n'est pas vide, renvoie le dernier élément et décrémente le sommet. */
-int depiler(PILE p)
+int depiler(PILE *p)
 {
-    if (pileVide(p) == 0) {
+    if (pileVide(&p) == 0) {
         int value = p.pile[p.sommet];
         p.sommet -= 1;
         return value;
@@ -33,9 +33,9 @@ int depiler(PILE p)
 }
 
 /* Si la pile n'est pas pleine, incrémente le sommet et ajoute l'élément à la pile. */
-int empiler(PILE p, int elt)
+int empiler(PILE *p, int elt)
 {
-    if(pilePleine(p) == 0) {
+    if(pilePleine(&p) == 0) {
         p.sommet += 1;
         p.pile[p.sommet] = elt;
         return 1;
@@ -86,9 +86,9 @@ int main()
             int valeur;
             printf("Entrez la valeur à empiler: ");
             scanf("%d", &valeur);
-            empiler(p, valeur) == 1 ? printf("La valeur %d ajoutée à la pile.\n", valeur) : printf("La pile est pleine !\n");
+            empiler(&p, valeur) == 1 ? printf("La valeur %d ajoutée à la pile.\n", valeur) : printf("La pile est pleine !\n");
         } else if (result == 3) {
-            depiler(p);
+            depiler(&p);
             printf("Une valeur à été retiré de la pile.\n");
 
         }
