393f23079abf3e8b86f7e18304068501711de489
flaw9 <contact@flaw.dev>
2021-12-17 11:41:34 +0100

    ...

--- initDev/C_course/td13/fact.c
+++ initDev/C_course/td13/fact.c
@@ -84,13 +84,11 @@ int mainP(int estCree)
     return request;
 }
 
-int fact(int n){
-    if (n == 1) {
+long int fact(int n) {
+    if (n>=1)
+        return n*fact(n-1);
+    else
         return 1;
-    }
-    else {
-        return n∗fact(n−1);
-    }
 }
 
 int main()
