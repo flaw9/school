83072c8e10c69871202464398f7ccbc0bd634af6
flaw9 <contact@flaw.dev>
2022-01-12 09:05:32 +0100

    ...

---
+++ initDev/C_course/tp27-28/identite.h
@@ -0,0 +1,46 @@
+#include <stdbool.h>
+
+typedef struct
+{
+    char *nom;
+    char *prenom;
+} Identite;
+
+// allocation d'une Identite
+// @param
+// nom, prenom : chaînes de caractère qui seront stockées
+// @return
+// la nouvelle Identite
+// renvoie(Identite *)NULL si om ou prenom sont des pointeurs invalides ou en cas d'échec d'allocation mémoire
+// @note
+// nom et prenom sont copiés par deep copy
+extern Identite *identiteNew(char *nom, char *prenom);
+
+// libération mémoire d'une Identite
+// @paam
+// id: l'Identite à libérer
+// @return (Identite *)NULL
+// fonctionne même si id, id->nom ou id->prenom sont nuls
+extern Identite *identiteDelete(Identite *id);
+
+// vérifie la validité d'une Identite
+// @param
+// id: l'Identite à vérifier
+// @return
+// false si l'Identite est invalide, càd id->nom ou id->prenom est NULL
+// true sinon
+extern bool identiteIsValid(Identite *id);
+
+// affiche une Identite sous la forme
+// "Nom: <nom> ; Prenom: <prenom>"
+// @param
+// id: l'Identite à afficher
+// @return Rien
+// @note affiche "Identite invalide" si l'Identite est invalide
+extern void identitePrint(Identite *id);
+
+// compare deux Identites
+// @param
+// a,b : les Identites à comparer
+// @return
+//
